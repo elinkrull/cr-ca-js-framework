21b94ebc8a17e9f5572f17029c07ad153585b724
elinjakobsen <126721502+elinkrull@users.noreply.github.com>
2025-03-13 18:32:06 +0100

    install react-router-dom, add links to navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import { ShoppingCart } from "lucide-react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Header() {
 	return (
@@ -8,12 +8,12 @@ export default function Header() {
 		<p>
 		HEADER
 		</p>
-		{/* <Link className="nav-link" to="/">HOME</Link>
+		<Link className="nav-link" to="/">HOME</Link>
 		<nav className="navbar">
      		<Link to="/" className="nav-link">PRODUCTS</Link>
-    		<Link to="/contact" className="nav-link">CONTACT</Link>
+    		<Link to="./pages/ContactPage" className="nav-link">CONTACT</Link>
     	</nav>
-		<img src="#" className="cart-icon" alt="Cart Icon" /> */}
+		<img src="#" className="cart-icon" alt="Cart Icon" />
 		<ShoppingCart className="shopping-cart" />
 		</header>
 	)
